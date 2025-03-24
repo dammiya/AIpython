2bfc374fc12bec4a8bd97f212ad15561b1d3ba30
dammi Kim <mcddam234@gmail.com>
2025-03-24 03:24:27 +0000

    연산자를 활용

---
+++ test.py
@@ -0,0 +1,8 @@
+# number = int(input("구구단을 출력할 숫자를 입력하세요 (예:2): ))
+number = input("구구단을 출력할 숫자를 입력하세요 (예:2): ")
+
+# print(f"{number}단:")
+print(f"{number}단:")
+# print(number, "단:")
+# print(number + "단:")
+# print(number)
