6a7b7fdcd98730aeff70a504fd3a1eaf7769973d
dammi Kim <mcddam234@gmail.com>
2025-03-31 12:18:20 +0000

    아스키코드

--- myif.py
+++ myif.py
@@ -1,57 +1,54 @@
 # 아스키 코드 그림 출력 하기
-def print_cat():
-    
-    cat = [
-       " /\_/\ "
-       "( o.o ) "
-       " >   < "
+def print_duck():
+    duck = [
+          " _",
+         "<(o )",
+         " (  >)"
+
     ]
-    for line in cat:
-        print(line)
-print("그림 출력 프로그램")
-print("=======================")
-print("1. 고양이")
-
-
-print_cat()
-
-print("2. 강아지")
- print_dog():
     
-    dog = [
-        " / \__"
-       " (    @\___"
-       " /         O"
-       " /   (_____/"
-       " /_____/  U"
-    ]
-    for line in dog:
+    for line in duck:
         print(line)
 
-print("3. 토끼")
+def print_bear():
+    bear = [
+       " ʕ•ᴥ•ʔ ",
+       " /  つ つ "
+    ]
+
+    for line in bear:
+        print(line)
+
 def print_rabbit():
-    
-    rabbit ]
-        " / \__"
-       " (    @\___"def
-       " /         O"
-       " /   (_____/"
-       " /_____/  U"
+    rabbit = [
+        " (\(\ ",
+        " ( -.-)",
+        " o_( )( )"
     ]
-    for line in dog:
+    
+    for line in rabbit:
         print(line)
 
-print("=======================")
-n = int(input("선택:"))
 
+print("그림 출력 프로그램")
+print("=====================")
+print("1. 오리")
+print("2. 곰")
+print("3. 토 끼")
+print("=====================")
+n = int(input("선택: "))
 # 만약에 1을 입력하면 1번 캐릭터 출력
 if n == 1:
-    print("고양이 그림")
+    print("오리")
+    print_duck()
 # 만약에 2를 입력하면 2번 캐릭터 출력
-elif n ==2:
-    print("강아지 그림")
+elif n == 2:
+    print("곰")
+    print_bear()
 # 3을 입력하면 3번 캐릭터 출력
-elif n ==3:
-    print("토끼 그림")
+elif n == 3:
+    print("토끼")
+    print_rabbit()
 # 잘못입력하면 잘못 입력했다고 출력
-elif 
+else:
+    print("잘못입력")
